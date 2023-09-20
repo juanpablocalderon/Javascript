15c10169b498049c3a395d9a71420ede5ae97209
Juanpablocalderon <juanpablocalderon79@gmail.com>
2023-09-20 01:54:55 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("hola mundo");
+console.log("Juan Pablo 031278");
+console.log("Mary Stuart (1542)"); console.log("Mary", "Stuart", "(1542)");
+console.log("Juan" + "Pablo" + "(1978)");
+console.log("juan ","pablo ","1978")
