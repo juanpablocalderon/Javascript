eab26c226950c939754f86540fb5f0ed94d27c57
Juanpablocalderon <juanpablocalderon79@gmail.com>
2023-09-20 03:37:23 -0500

    actualizacion

---
+++ 02-operadores_logicos/operadores.js
@@ -0,0 +1,10 @@
+var edad = 18;
+var dni = true;
+
+//ejemplo AND - Y
+console.log (edad >= 18 && dni == true);
+
+console.log("==========")
+
+//ejemplo OR - o
+console.log (edad >= 18 || dni == true);
