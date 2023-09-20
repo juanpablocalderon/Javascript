499cd778dbc8f00da7d038757fb8b2cc26189b3c
Juanpablocalderon <juanpablocalderon79@gmail.com>
2023-09-20 02:28:26 -0500

    actualizacion

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-console.log("hola mundo");
-console.log("Juan Pablo 031278");
-console.log("Mary Stuart (1542)"); console.log("Mary", "Stuart", "(1542)");
-console.log("Juan" + "Pablo" + "(1978)");
-console.log("juan ","pablo ","1978")
+var nombre = "Juan Pablo";
+var edad = "30";
+var presenteOno = true;
+var apellido = "depaula";
+var estatura = 1.70;
+var metros = "mts"
+
+console.log ("nombre: " + nombre);
+console.log ("edad: " + edad);
+console.log ("apellido: " + apellido);
+console.log ("estura: " + estatura + metros);
+
+
+//alert("Nombre " + nombre);
