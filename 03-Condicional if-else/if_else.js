5865af0050a55e8c052f3c81aa4341adffacf539
Juanpablocalderon <juanpablocalderon79@gmail.com>
2023-09-21 02:56:01 -0500

    Actualizacion

---
+++ 03-Condicional if-else/if_else.js
@@ -0,0 +1,12 @@
+var aprobre = false;
+
+if(aprobre == true){
+    console.log ("puedo salir");
+    alert ("puedo salir");
+    
+}
+else{
+    console.log("tengo que estudiar");
+    alert("tengo que estudiar");
+    
+}
---
+++ 04-Condicional switch/switch.js
@@ -0,0 +1,24 @@
+var color = prompt("Escribir un color primario");
+switch(color){
+
+    case"rojo":    
+    console.log("El color es rojo");
+    alert("El color es rojo")    
+    break;
+
+    case"azul":
+    console.log("El color es azul");
+    alert("El color es azul")
+    break;
+
+    case"amarillo":
+    console.log("El color es amarillo");
+    alert("El color es amarillo")
+    break;
+
+    default:
+    console.log("El color no es valido");
+    alert("El color no es valido")
+    break;
+
+}
