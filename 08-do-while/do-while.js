10854301ab9597966c51b7574275fe8365446d7c
Juanpablocalderon <juanpablocalderon79@gmail.com>
2023-10-15 04:49:08 -0500

    Actualizacion

--- 08-do-while/do-while.js
+++ 08-do-while/do-while.js
@@ -4,3 +4,39 @@ do{
     contador ++;
 }
 while(contador<10)
+
+
+if (contador <= 10 && show === false) {
+    console.log("test");
+}
+
+
+for (let x = 10; x > 1; x -= 2)
+console.log("hello");
+
+
+let animals = ["dog", "cat", "hamster", "rabbit"];
+for (let i = 0; i < animals.length; i+=2)
+     console.log(animals[i]);
+
+for(let indice: animals)
+    console.log(indice)
+
+
+let a = "test"
+     for (let a = 4; a < 4; a++) {
+        console.log("test");
+    }
+
+    let car = {make: "Citroen", model: "DS"};
+    for (let f in car)
+    console.log(f);
+
+    
+    let steps = [3, 2, 1];
+    for (let n of steps)
+    console.log(n);
+    
+    
+
+
---
+++ 09-Ejercicios/ejercicio_cuenta regresiva/cuenta_regreseva.js
@@ -0,0 +1,4 @@
+for(var i=10; i>=0; i--){
+    console.log(i);
+}
+console.log("Feliz año nuevo");
